0ab22b1744eb85d7bd2605bd20d42b47d58d5f9d
Genki Nukazuka <nukadukagenki@gmail.com>
2015-10-26 23:38:15 +0100

    ?

--- include/MyLibrary.hh
+++ include/MyLibrary.hh
@@ -16,11 +16,11 @@
 
 //if __CINT__ is defined, include its source files
 #ifdef __CINT__
-#include "/afs/cern.ch/user/g/gnukazuk/private/MyLibrary/src/../src/CanvasOperation.cc"
-#include "/afs/cern.ch/user/g/gnukazuk/private/MyLibrary/src/../src/IoOperation.cc"
-#include "/afs/cern.ch/user/g/gnukazuk/private/MyLibrary/src/../src/NumericalOperation.cc"
-#include "/afs/cern.ch/user/g/gnukazuk/private/MyLibrary/src/../src/PaveOperation.cc"
-#include "/afs/cern.ch/user/g/gnukazuk/private/MyLibrary/src/../src/StringOperation.cc"
-#include "/afs/cern.ch/user/g/gnukazuk/private/MyLibrary/src/../src/Style.cc"
+#include "/Users/nukazuka/local/MyLibrary/src/../src/CanvasOperation.cc"
+#include "/Users/nukazuka/local/MyLibrary/src/../src/IoOperation.cc"
+#include "/Users/nukazuka/local/MyLibrary/src/../src/NumericalOperation.cc"
+#include "/Users/nukazuka/local/MyLibrary/src/../src/PaveOperation.cc"
+#include "/Users/nukazuka/local/MyLibrary/src/../src/StringOperation.cc"
+#include "/Users/nukazuka/local/MyLibrary/src/../src/Style.cc"
 #endif // __CINT__
 #endif // MYLIB_MYLIBRARY_HH
