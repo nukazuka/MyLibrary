ada0078cd9535cb6cb61f888306bab4b1bbeaf4b
Genki Nukazuka <nukadukagenki@gmail.com>
2015-12-16 13:17:56 +0100

    TODO testoooo

--- include/MultiHist.hh
+++ include/MultiHist.hh
@@ -12,6 +12,7 @@
   @date 26/Nov./2015
   @author G. Nukazuka
   @details This class handle some histograms at same time.
+  @TODO totototototdododododdo
 */
 
 using namespace std;
@@ -60,6 +61,7 @@ public:
   /*!
     @fn MultiHist()
     @brief A default constructor.
+    @TODO aaaaaaaaaaaaaaaaaa
   */
   MultiHist(){ Init();};
 
