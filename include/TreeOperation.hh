683070a0b601d0dfb9787d7a5e19a2d2d9b0cc51
Genki Nukazuka <nukadukagenki@gmail.com>
2016-06-26 17:23:12 +0200

    TreeOperation.hh was added

---
+++ include/TreeOperation.hh
@@ -0,0 +1,20 @@
+#ifndef MYLIB_TREEOPERATION
+#define 
+
+#ifdef __linux__
+#include "/home/gnukazuk/local/MyLibrary/include/MyLibrary.hh"
+#elif __APPLE__
+#include "/Users/nukazuka/local/MyLibrary/include/MyLibrary.hh"
+#endif
+
+//#include ".hh"
+
+using namespace std;
+
+template < class T >
+T* GetHist( TTree* tr, string name, string title,
+	    int bin_num, 
+	    string target, string cut )
+
+
+#endif
