f78d9586357d57b5955c5a4fb504667e89336f53
Genki Nukazuka <nukadukagenki@gmail.com>
2016-07-11 12:50:29 +0200

    TreeOperation を殺した。もう少しまともに書かないとエラーを作ってしまう。MyLibrary.hh にも影響を与える。とりあえず include しなければ大丈夫。早めに直す。

--- include/TreeOperation.hh
+++ include/TreeOperation.hh
@@ -1,20 +1,22 @@
 #ifndef MYLIB_TREEOPERATION
-#define 
+#define  MYLIB_TREEOPERATION
 
+/*
 #ifdef __linux__
 #include "/home/gnukazuk/local/MyLibrary/include/MyLibrary.hh"
 #elif __APPLE__
 #include "/Users/nukazuka/local/MyLibrary/include/MyLibrary.hh"
 #endif
+*/
 
 //#include ".hh"
-
+/*
 using namespace std;
 
 template < class T >
 T* GetHist( TTree* tr, string name, string title,
 	    int bin_num, 
 	    string target, string cut )
-
+*/
 
 #endif
